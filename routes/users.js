fcabd05c04d8ac000120f496450107cb28b0061c
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-04 22:29:36 +0300

    error form changed

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,18 @@
+const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('express').Router();
 const { getUsers, getUser } = require('../controllers/users');
+const NotFoundError = require('../errors/not-found-err');
 
-usersRouter.get('/users/:id', getUser);
+usersRouter.get('/users/:id', celebrate({
+  body: Joi.object().keys({
+      id: Joi.string().required().min(1),
+  }),
+}), getUser);
 
 usersRouter.get('/users', getUsers);
 
-usersRouter.get('/:nonexistent', (req, res) => {
-	res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
+usersRouter.get('/:nonexistent', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
 
 module.exports = usersRouter;
