05baf0b296cbadf66e9fb15e58a161c0440296a9
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-04 22:35:01 +0300

    celebrate validation

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,10 @@
 const { celebrate, Joi } = require('celebrate');
+const { getUserObj } = require('./celebrate_obj/celebrate-obj');
 const usersRouter = require('express').Router();
 const { getUsers, getUser } = require('../controllers/users');
 const NotFoundError = require('../errors/not-found-err');
 
-usersRouter.get('/users/:id', celebrate({
-  body: Joi.object().keys({
-      id: Joi.string().required().min(1),
-  }),
-}), getUser);
+usersRouter.get('/users/:id', celebrate(getUserObj), getUser);
 
 usersRouter.get('/users', getUsers);
 
