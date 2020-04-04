852aed30e85c892f8a0e50085decea61f45faea3
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-04 22:34:21 +0300

    celebrate validation

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
 const { createUser, login } = require('./controllers/users.js');
+const { loginObj, createUserObj, authObj } = require('./celebrate_obj/celebrate-obj');
 const auth = require('./middlewares/auth.js');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -15,14 +17,31 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 	useFindAndModify: false,
 })
 	.then(() => console.log('Соединение с базой данных установлено'))
-	.catch((err) => console.log(err.message));
-
+	.catch((err) => console.log(err.message)); //не знаю что делать с этой ошибкой,
+// тут ведь никак не впилить next
 app.use(bodyParser.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
-app.use('/', auth, cardsRouter);
-app.use('/', auth, usersRouter);
+app.post('/signin', celebrate(loginObj), login);
+
+app.post('/signup', celebrate(createUserObj), createUser);
+
+app.use('/', celebrate(authObj), auth, cardsRouter);
+
+app.use('/', celebrate(authObj), auth, usersRouter);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+      .status(statusCode)
+      .send({
+          message: statusCode === 500
+              ? 'На сервере произошла ошибка'
+              : message
+      });
+});
 
 app.listen(PORT, () => {
 	// console.log('Im running');
