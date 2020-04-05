ad773de29db55f84e641073d7aa45c3a07c86004
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-05 04:08:46 +0300

    fix validation

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,6 @@ function getAuth(req) {
 	if (!authorization || !authorization.startsWith('Bearer ')) {
 		throw new AuthReqError('Для выполнения действия необходима авторизация');
 	}
-
 	return authorization;
 }
 
