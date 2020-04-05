bf3ffdacca0f42dd99dfa15d357a14682bbfc274
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-05 12:58:51 +0300

    error behavior fixed

--- middlewares/cards.js
+++ middlewares/cards.js
@@ -3,22 +3,28 @@ const NotFoundError = require('../errors/not-found-err');
 const PermReqError = require('../errors/perm-required-err');
 
 module.exports.doesCardExist = (req, res, next) => {
+	console.log('DOES EXIST');
 	Card.findById(req.params.cardId)
 		.then((card) => {
+			console.log('ITS HERE');
+			console.log(card);
 			if (!card) {
 				throw new NotFoundError('Карточка, которую вы пытаетесь удалить, не найдена');
 			} else {
-				return true;
+				next();
 			}
 		})
 		.catch((err) => {
+			console.log('NOT HERE');
 			next(err);
 		});
 };
 
 module.exports.checkOwner = (req, res, next) => {
+	console.log('IS OWNER');
 	Card.findById(req.params.cardId)
 		.then((card) => {
+			console.log(req.user._id.toString());
 			if (card.owner.toString() !== req.user._id.toString()) {
 				throw new PermReqError('Недостаточно прав');
 			} else {
